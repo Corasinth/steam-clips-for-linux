b9bf590b09a363be839d9a1271757dd3492d03e8
unknown <beforeyouknowkindness@gmail.com>
2026-03-10 01:09:21 -0500

    finish script

--- steam_script.sh
+++ steam_script.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
+# Path = Steam game recording location
+# Destination = where the clips end up
+PATH="~/.steam/debian/-installations/userdata/434129136/gamerecordings"
+DESTINATION="~/Videos"
 
-
-
-cat init-stream0.m4s $(ls -vx chunk-stream0*.m4s) > stream0.mp4
-cat init-stream1.m4s $(ls -vx chunk-stream1*.m4s) > stream1.mp4
-ffmpeg -i stream0.mp4 -i stream1.mp4 -c copy final.mp4
-rm stream0.mp4 stream1.mp4
-
-mv final.mp4 ~/Desktop/Videos/
+cd $PATH
+for dir in */; do
+    # echo "$dir"
+    cd $dir
+    cat init-stream0.m4s $(ls -vx chunk-stream0*.m4s) > stream0.mp4
+    cat init-stream1.m4s $(ls -vx chunk-stream1*.m4s) > stream1.mp4
+    ffmpeg -i stream0.mp4 -i stream1.mp4 -c copy $dir.mp4
+    # rm stream0.mp4 stream1.mp4
+    mv $dir.mp4 $DESTINATION/$dir.mp4
+    cd ../
+    rm $dir
+done
